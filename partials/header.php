e0047358377a9a01c8d2394a88853c1bd688a2f1
burillu <burillu90@hotmail.it>
2023-12-01 15:25:11 +0100

    add content in css and import in header

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,5 @@
+<?php include __DIR__ . '/../functions/function.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +17,7 @@
         crossorigin="anonymous" referrerpolicy="no-referrer">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="../css/style.css" type="text/css"> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.0/axios.min.js"
         integrity="sha512-WrdC3CE9vf1nBf58JHepuWT4x24uTacky9fuzw2g/3L9JkihgwZ6Cfv+JGTtNyosOhEmttMtEZ6H3qJWfI7gIQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -24,6 +26,10 @@
 </head>
 
 <body>
+    <style>
+        <?php
+        include __DIR__ . '/../css/style.css'; ?>
+    </style>
 
     <div id="app">
         <header>
